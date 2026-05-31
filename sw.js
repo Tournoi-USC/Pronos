76995ea662eb46e777eeedc59e27fa85a0877931
Tournoi-USC <us-croissy@outlook.fr>
2026-05-31 14:08:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,9 @@
-const CACHE = 'prono-cdm26-v6';
-const LOCAL = ['./index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(LOCAL)));
-  self.skipWaiting();
-});
-
+// Service Worker désactivé - se désinstalle automatiquement
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))
+    .then(() => self.registration.unregister())
   );
   self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-  // Ne jamais intercepter les requêtes externes (CDN, Supabase, APIs)
-  if (url.origin !== self.location.origin) return;
-  // Pour les fichiers locaux : cache first
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
 });
